44bf000a0005e3625f976f10bc3119a8ff7d9eaf
lsfckt <132505560+lsfckt@users.noreply.github.com>
2024-07-11 22:47:43 +0300

    add top area navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import TopArea from "./components/TopArea";
 
 function App() {
 
   return (
     <>
       <Header />
+      <TopArea />
       <Footer />
     </>
   )
