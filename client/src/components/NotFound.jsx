8bbc1541f4cc440eb975f4dba0f45cf27dae58a9
lsfckt <132505560+lsfckt@users.noreply.github.com>
2024-07-13 00:51:22 +0300

    create 404 page component

---
+++ client/src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import styles from './NotFound.module.css';
+
+export default function NotFound() {
+
+    return (
+        <div className={styles.media}>
+            <img src="assets/images/404.jfif" alt="404" />
+        </div>
+
+    )
+}
