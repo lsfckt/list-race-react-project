dd0e7872206b0eb2457cabcdabe5cd922892bc72
lsfckt <132505560+lsfckt@users.noreply.github.com>
2024-07-15 23:39:40 +0300

    edit password input placeholder

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -6,10 +6,10 @@ export default function Register() {
         <div className={styles['create-pass-layout']}>
             <h3 className={styles['create-pass-title']}>Create your password</h3>
             <p>Use a minimum of 10 symbols, including upper and lower case letters and numbers.</p>
-
+ 
             <form className={styles['create-pass-form']}>
                 <label htmlFor="password">
-                    <input className={styles['password']} type="password" name="password" id="password" placeholder="Enter your password"/>
+                    <input className={styles['password']} type="password" name="password" id="password" placeholder="Create your password"/>
                 </label>
                 <label htmlFor="confirm-password">
                     <input className={styles['confirm-password']} type="password" name="confirm-password" id="confirm-password" placeholder="Confirm your password"/>
