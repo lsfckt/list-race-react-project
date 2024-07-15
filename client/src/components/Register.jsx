6ce7eeb749fbdcd54bb441ea5bb45bc767e65198
lsfckt <132505560+lsfckt@users.noreply.github.com>
2024-07-15 23:01:09 +0300

    create register component

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,22 @@
+import styles from './Register.module.css';
+
+export default function Register() {
+
+    return (
+        <div className={styles['create-pass-layout']}>
+            <h3 className={styles['create-pass-title']}>Create your password</h3>
+            <p>Use a minimum of 10 symbols, including upper and lower case letters and numbers.</p>
+
+            <form className={styles['create-pass-form']}>
+                <label htmlFor="password">
+                    <input className={styles['password']} type="password" name="password" id="password" placeholder="Enter your password"/>
+                </label>
+                <label htmlFor="confirm-password">
+                    <input className={styles['confirm-password']} type="password" name="confirm-password" id="confirm-password" placeholder="Confirm your password"/>
+                </label>
+            </form>
+
+            <button type="submit" className={styles['create-btn-submit']}>Create Profile</button>
+        </div>
+    )
+}
