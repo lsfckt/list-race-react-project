913fbac782074614b6d87dd40094178911413c6c
lsfckt <132505560+lsfckt@users.noreply.github.com>
2024-07-20 14:25:28 +0300

    edit register component
    add email input

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -3,17 +3,23 @@ import styles from './Register.module.css';
 export default function Register() {
 
     return (
-        <div className={styles['create-pass-layout']}>
-            <h3 className={styles['create-pass-title']}>Create your password</h3>
+        <div className={styles['create-account-layout']}>
+            <h3 className={styles['create-account-title']}>Create your password</h3>
             <p>Use a minimum of 10 symbols, including upper and lower case letters and numbers.</p>
- 
-            <form className={styles['create-pass-form']}>
-                <label htmlFor="password">
-                    <input className={styles['password']} type="password" name="password" id="password" placeholder="Create your password"/>
-                </label>
-                <label htmlFor="confirm-password">
-                    <input className={styles['confirm-password']} type="password" name="confirm-password" id="confirm-password" placeholder="Confirm your password"/>
-                </label>
+
+            <form className={styles['create-account-form']}>
+                <div className={styles['labels']}>
+                    <label htmlFor="email">Email</label>
+                    <input className={styles['email']} type="email" name="email" id="email" placeholder="Write your email" />
+                </div>
+                <div className={styles['labels']}>
+                    <label htmlFor="password">Password</label>
+                    <input className={styles['password']} type="password" name="password" id="password" placeholder="Create your password" />
+                </div>
+                <div className={styles['labels']}>
+                    <label htmlFor="confirm-password">Confirm Password</label>
+                    <input className={styles['confirm-password']} type="password" name="confirm-password" id="confirm-password" placeholder="Confirm your password" />
+                </div>
             </form>
 
             <button type="submit" className={styles['create-btn-submit']}>Create Profile</button>
