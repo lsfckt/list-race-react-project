42a90c7678c1d7e7db97befd12aa37dd5b3c9bfd
lsfckt <stoyanovkt@gmail.com>
2024-07-20 15:56:47 +0300

    crearte api folder and requester function for fetching data

---
+++ client/src/api/requester.js
@@ -0,0 +1,21 @@
+export default async function requester(method, url) {
+
+    const options = {};
+
+    if (method !== 'GET') {
+        options.method = method;
+    }
+
+    if (data) {
+        options.headers = {
+            'Content-Type': 'application/json',
+        };
+
+        options.body = JSON.stringify(data);
+    }
+
+    const response = await fetch(url, options);
+    const result = await response.json();
+
+    return result;
+}
