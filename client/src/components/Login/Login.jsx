2176ab9d11ab42d21634914a6bbd8e089077c877
lsfckt <stoyanovkt@gmail.com>
2024-07-23 23:17:18 +0300

    implement login functionality

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,18 +1,60 @@
+import { useEffect, useState } from 'react';
+
 import styles from './Login.module.css';
+import * as requester from '../../api/requester';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Login() {
 
-    return (
-        <div className={styles['enter-pass-layout']}>
-            <h3 className={styles['enter-pass-title']}>Enter your password</h3>
+    const [submitClicked, setSubmitClicked] = useState(false);
+    const [formData, setFormData] = useState({});
 
-            <form className={styles['enter-pass-form']}>
-                <label htmlFor="password">
-                    <input className={styles['password']} type="password" name="password" id="password" placeholder="Enter your password"/>
-                </label>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmitClicked(true);
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        })
+    }
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        (async () => {
+
+            if (submitClicked) {
+                const user = await requester.post('http://localhost:3030/users/login', { ...formData })
+                navigate('/');
+            }
+
+            setSubmitClicked(false);
+        })();
+
+    }, [submitClicked]);
+
+    return (
+        <div className={styles['login-layout']}>
+            <h3 className={styles['login-title']}>Login to your account</h3>
+
+            <form className={styles['login-form']} onSubmit={handleSubmit}>
+                <div className={styles['labels']}>
+                    <label htmlFor="email">Email</label>
+                    <input className={styles['email']} type="email" name="email" id="email" placeholder="Enter your email" onChange={handleChange} />
+                </div>
+                <div className={styles['labels']}>
+                    <label htmlFor="password">Password</label>
+                    <input className={styles['password']} type="password" name="password" id="password" placeholder="Enter your password" onChange={handleChange} />
+                </div>
+
+                <button type="submit" className={styles['login-btn-submit']}>Login</button>
             </form>
 
-            <button type="submit" className={styles['login-btn-submit']}>Login</button>
         </div>
     )
 }
