2abb3e003474ea66e1813ef3f771a10d79e779c7
lsfckt <stoyanovkt@gmail.com>
2024-09-25 16:19:02 +0300

    Hide password from app state

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -3,15 +3,17 @@ import { useContext } from "react";
 import { login, register } from "../api/auth-api";
 import { AuthContext } from "../contexts/AuthContext";
 
+// TODO: Hide password from result return
+
 export const useLogin = () => {
     const { changeAuthState } = useContext(AuthContext);
 
     const loginHandler = async (email, password) => {
-        const result = await login(email, password);
+        const { password: _, ...authData } = await login(email, password);
 
-        changeAuthState(result);
+        changeAuthState(authData);
 
-        return result;
+        return authData;
     }
 
     return loginHandler;
@@ -21,11 +23,11 @@ export const useRegister = () => {
     const { changeAuthState } = useContext(AuthContext);
 
     const registerHandler = async (email, password) => {
-        const result = await register(email, password);
+        const { password: _, ...regData } = await register(email, password);
 
-        changeAuthState(result);
+        changeAuthState(regData);
 
-        return result;
+        return regData;
 
     }
     return registerHandler;
